6f24e2b5ce73b4547c7df9fc6388571b11cffde8
gfayette <George.Fayette@meijer.com>
2022-04-26 20:14:43 -0400

    Homepage edit

--- src/Website/Components/Home.js
+++ src/Website/Components/Home.js
@@ -42,12 +42,13 @@ export default class Home extends React.Component {
                     </p>
                     <p style={textcss}>
                         My name is George. I'm a software engineer with experience building applications ranging from small websites for local businesses, 
-                        to enterprise systems for large corporations. I'm a skilled coder across all areas of the modern technology stack. 
-                        From front-end to back-end, low-level system programming to high-level object-oriented designs - I just love to code!
+                        to enterprise systems for large corporations. I've worked across many areas of the modern technology stack, including front-end mobile and web development, 
+                        back-end APIs and databases, low-level systems programming, and high-level object-oriented applications.
+
                     </p>
                     <p style={textcss}>
-                        You'll have to forgive the lack of additional content here at the moment. This site was mainly designed to demonstrate my competence with 
-                        JavaScript, HTML, CSS, WebGL, and screen compatibility.
+                        This site was primarily designed to demonstrate my competence in 
+                        JavaScript, HTML, CSS, and WebGL.
                         If you'd like additional information about my skillset and professional experience, feel free to view my résumé.
                     </p>
                 </div>
